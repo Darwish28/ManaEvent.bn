21ca68226abbc97c714b16e154bb02e5c334648f
Aiman Darwish <24ftt1090@student.pb.edu.bn>
2025-10-08 09:31:13 +0800

    final touch of the admin page's color scheme

--- resources/js/Admin/Pages/PublishEvent.tsx
+++ resources/js/Admin/Pages/PublishEvent.tsx
@@ -290,7 +290,7 @@ const PublishEvent = () => {
             <button
               type="submit"
               disabled={isSubmitting}
-              className={`inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white ${isSubmitting ? 'bg-blue-400' : 'bg-blue-600 hover:bg-blue-700'} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
+              className={`inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white ${isSubmitting ? 'bg-blue-400' : 'bg-yellow-400 hover:bg-yellow-500'} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
             >
               {isSubmitting ? 'Publishing...' : 'Publish Event'}
             </button>
