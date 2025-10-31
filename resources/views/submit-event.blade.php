8ef2b5e016129e8b1a2562695519379727466c95
lyvssoul <24ftt1080@student.pb.edu.bn>
2025-11-01 00:04:13 +0800

    fixing the submit event page

--- resources/views/submit-event.blade.php
+++ resources/views/submit-event.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.app')
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ManaEvent • Submit Your Event</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
 
 @section('content')
 <header class="bg-yellow-400 p-4 flex items-center justify-between relative">
