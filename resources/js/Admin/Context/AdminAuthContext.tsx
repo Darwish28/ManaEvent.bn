0a899d0af359a679c9fb1bbc1ba2c9487e9a7f03
Aiman Darwish <24ftt1090@student.pb.edu.bn>
2025-11-06 11:07:56 +0800

    vps again

--- resources/js/Admin/Context/AdminAuthContext.tsx
+++ resources/js/Admin/Context/AdminAuthContext.tsx
@@ -35,7 +35,7 @@ export const AdminAuthProvider = ({ children }: { children: React.ReactNode }) =
 
   axios.defaults.withCredentials = true;
   axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://manaeventbn.duckdns.org';
 
   // ✅ Verify Laravel session on app load
   useEffect(() => {
@@ -79,6 +79,7 @@ export const AdminAuthProvider = ({ children }: { children: React.ReactNode }) =
       return false
     }
   }
+console.log('🛰️ Axios Base URL:', axios.defaults.baseURL);
 
   // ✅ Logout
   const logout = async (): Promise<void> => {
